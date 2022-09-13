7b004be9da65322a45235bfe80ef8f03de4bb00d
Carlos Osuna <carlos.osuna@vanco.com>
2022-09-13 00:26:16 -0500

    need to see what's inside

--- s3upload.js
+++ s3upload.js
@@ -6,19 +6,23 @@ const s3 = new AWS.S3();
 
 module.exports.handle = async (event) => {
     try {
-      const { filename, data } = extractFile(event)
-      await s3.putObject({ Bucket: BUCKET, Key: filename, ACL: 'public-read', Body: data }).promise();
-
+      //const { filename, data } = extractFile(event)
+      //await s3.putObject({ Bucket: BUCKET, Key: filename, ACL: 'public-read', Body: data }).promise();
       return {
         statusCode: 200,
-        body: JSON.stringify({ link: `https://${BUCKET}.s3.amazonaws.com/${filename}` })
+        body:  extractFile(event)
       }
+
+    //   return {
+    //     statusCode: 200,
+    //     headers: {
+    //         "Access-Control-Allow-Origin": "*"
+    //     },
+    //     body: JSON.stringify({ link: `https://${BUCKET}.s3.amazonaws.com/${filename}` })
+    //   }
     } catch (err) {
       return {
         statusCode: 500,
-        headers: {
-            "Access-Control-Allow-Origin": "*"
-        },
         body: JSON.stringify({ message: err.stack })
       }
     }
