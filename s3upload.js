384fe732006eaa4445acf2c8cfd58b79ea4e41c1
Carlos Osuna <carlos.osuna@vanco.com>
2022-09-13 00:54:55 -0500

    new approach

--- s3upload.js
+++ s3upload.js
@@ -1,43 +1,39 @@
 const AWS = require('aws-sdk');
-const multipart = require('aws-lambda-multipart-parser');
 
-const BUCKET = process.env.BUCKET;
+
+const BUCKET_NAME = process.env.BUCKET;
 const s3 = new AWS.S3();
 
 module.exports.handle = async (event) => {
-    try {
-      //const { filename, data } = extractFile(event)
-      //await s3.putObject({ Bucket: BUCKET, Key: filename, ACL: 'public-read', Body: data }).promise();
-      return {
+    console.log(event);
+
+    const response = {
+        isBase64Encoded: false,
         statusCode: 200,
-        body:  JSON.stringify(extractFile(event))
-      }
+        body: JSON.stringify({ message: "Successfully uploaded file to S3" }),
+    };
 
-    //   return {
-    //     statusCode: 200,
-    //     headers: {
-    //         "Access-Control-Allow-Origin": "*"
-    //     },
-    //     body: JSON.stringify({ link: `https://${BUCKET}.s3.amazonaws.com/${filename}` })
-    //   }
-    } catch (err) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ message: err.stack })
-      }
+    try {
+        const parsedBody = JSON.parse(event.body);
+        const base64File = parsedBody.file;
+        const decodedFile = Buffer.from(base64File.replace(/^data:image\/\w+;base64,/, ""), "base64");
+        const params = {
+            Bucket: BUCKET_NAME,
+            Key: `images/${new Date().toISOString()}.jpeg`,
+            Body: decodedFile,
+            ContentType: "image/jpeg",
+        };
+
+        const uploadResult = await s3.upload(params).promise();
+
+        response.body = JSON.stringify({ message: "Successfully uploaded file to S3", uploadResult });
+    } catch (e) {
+        console.error(e);
+        response.body = JSON.stringify({ message: "File failed to upload", errorMessage: e });
+        response.statusCode = 500;
     }
-  }
 
-  function extractFile(event) {
-    // spotText === true response file will be Buffer and spotText === false: String
-    spotText = true;
-    const result = multipart.parse(event, spotText) 
-    // const filename = result.file.filename;
-    // const data = result.file.content.data;
+    return response;
+  
+}
 
-    // return {
-    //   filename,
-    //   data
-    // }
-    return result;
-  }
