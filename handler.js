d1b1f4a1953e2d1336f517bdb016acec5770da5a
Carlos Osuna <carlos.osuna@vanco.com>
2022-09-12 22:21:18 -0500

    added functions

--- handler.js
+++ handler.js
@@ -1,15 +1,34 @@
-'use strict';
-
-module.exports.hello = async (event) => {
+const AWS = require('aws-sdk');
+const parseMultipart = require('parse-multipart');
+ 
+const BUCKET = process.env.BUCKET;
+ 
+const s3 = new AWS.S3();
+ 
+module.exports.handle = async (event) => {
+  try {
+    const { filename, data } = extractFile(event)
+     await s3.putObject({ Bucket: BUCKET, Key: filename, ACL: 'public-read', Body: data }).promise();
+ 
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ link: `https://${BUCKET}.s3.amazonaws.com/${filename}` })
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: err.stack })
+    }
+  }
+}
+ 
+function extractFile(event) {
+  const boundary = parseMultipart.getBoundary(event.headers['content-type'])
+  const parts = parseMultipart.Parse(Buffer.from(event.body, 'base64'), boundary);
+  const [{ filename, data }] = parts
+ 
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-};
+    filename,
+    data
+  }
+}
